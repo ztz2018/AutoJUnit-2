8c45f76ac7534771a96fd51fce71dfc80731238f
anktkumar <ankitkumar@fptechscience.com>
2018-05-11 14:19:51 +0530

    A working example

--- src/main/java/parser/MainParser.java
+++ src/main/java/parser/MainParser.java
@@ -1,7 +1,25 @@
 package parser;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+
+import java.io.FileReader;
+import java.util.Optional;
+
 public class MainParser {
 
+    public static void main(String args[]) {
 
+        try {
+            CompilationUnit compilationUnit = JavaParser.parse(new FileReader("src/main/java/sample_project/MainClass.java"));
+            Optional<ClassOrInterfaceDeclaration> classMain = compilationUnit.getClassByName("MainClass");
+            classMain.get();
+            classMain.toString();
+        } catch (Exception e) {
+                    e.printStackTrace();
+        }
+
+    }
 
 }
